f7172640feb66a553121d194e704e5e10897490d
olegsergeev <olehrubchinskiy@gmail.com>
2017-11-21 21:17:30 +0300

    no message

---
+++ src/main/java/by/pvt/gl/servlets/MyServlet1.java
@@ -0,0 +1,40 @@
+package by.pvt.gl.servlets;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet1 extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String fio = req.getParameter("fio");
+		// String lastName = req.getParameter("lastName");
+		// int age = Integer.parseInt(req.getParameter("age"));
+		// Enumeration<String> params = req.getParameterNames();
+
+		// while (params.hasMoreElements()) {
+		// String string = (String) params.nextElement();
+		// System.out.println(string);
+		// }
+		resp.getWriter().print("Hello");
+
+		System.out.println(req.getHeader("User-Agent"));
+		System.out.println(req.getHeader("x-my-header"));
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("do Post");
+
+	}
+
+}
